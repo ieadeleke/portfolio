c4d4d40a8f1eb49e025e1ea6ef9ae1c242d4c325
ieadeleke <eadelekeife@yahoo.com>
2026-07-08 14:02:41 +0100

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import '@fontsource-variable/outfit/index.css'
-import '@fontsource-variable/big-shoulders-display/index.css'
-import './index.css'
-import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/next";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import '@fontsource-variable/outfit/index.css';
+import '@fontsource-variable/big-shoulders-display/index.css';
+import './index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
